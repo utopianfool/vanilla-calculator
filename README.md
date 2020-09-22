b6de02777fc72956f04359ed52ec104bdecf0afe
Phil <phil@evolvedigital.co.uk>
2020-09-22 16:33:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Calculator App
+
+Calculator App using vanilla javascript. 
+by The Utopian Fool
+
+🧠 Concepts Covered:
+ 
+- How to use ES6 classes to organize code
+- How to sync JavaScript code with a UI
+- CSS Grid
+- Flexbox
+- The best way to cleanly handle user input
+- How to debug complicated edge cases
+
+## Fix CORS issue on localhost
+
+Use this page as a proxy to fix CORS issue https://cors-anywhere.herokuapp.com/
+
+```
+
+/* Example in vanilla Javascript*/
+
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const url = `${proxy}https://github.com/utopianfool/link-api/projects.json`;
+
+```
+
+:globe_with_meridians:
+[Demo](https://calculator.utopianfool.co.uk)
+
+------------------------------------------------------
+
+with thanks to Web dev simplified
+https://www.youtube.com/watch?v=j59qQ7YWLxw&ab_channel=WebDevSimplified
+for the initial tutorials
---
+++ app.js
@@ -0,0 +1,208 @@
+/* Navigation */
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const social = document.querySelector('.social-links');
+    // get all nav links
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const socialLinks = document.querySelectorAll('.social-links a');
+
+    // Toggle nav
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+        social.classList.toggle('nav-active');
+
+        
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`; // add delay at end
+            }
+        });
+
+        socialLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`; // add delay at end
+            }
+        });
+
+        // Animate burger
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+/* Use fetch api to get list of projects hosted elsewhere */
+const projectsList = () => {
+
+    var address = config.api; // Use config.js to hide api
+    // Get json data from url
+    const proxy = 'https://cors-anywhere.herokuapp.com/'; // use proxy to fix cors on localhost
+    const url = `${proxy}${address}`;
+
+
+    fetch(url) 
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            // console.log(data);
+            appendData(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    function appendData(data) {
+        var subMenu = document.querySelector('.sub-menu');
+        for(var i = 0; i < data.length; i++) {
+            var listItem = document.createElement('li');
+            listItem.innerHTML = '<a target="_blank" rel="noopener" title="' + data[i].description + '" href="' + data[i].link_url + '">' + data[i].link_text + '</a>';
+            subMenu.appendChild(listItem);
+        }
+    } // https://howtocreateapps.com/fetch-and-display-json-html-javascript/
+
+}
+
+/* Caculator app */
+
+// Create calculator class
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number) {
+        // only show one .
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            });
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay;
+        }
+        
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+
+// keep it tidy by invoking smaller functions inside of app function
+const app = () => {
+    navSlide();
+    projectsList();
+}
+
+app();
